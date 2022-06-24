fab98f8f2a41a67e514d5036315c0cd03ab4b4a0
A20Karlikov <al.karlikov@gmail.com>
2022-06-24 14:00:02 +0300

    Movie Details Screen commit

--- app/src/main/java/com/io/moviO/MainActivity.kt
+++ app/src/main/java/com/io/moviO/MainActivity.kt
@@ -2,10 +2,19 @@ package com.io.moviO
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.io.moviO.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val movieDetails = MovieDetailsFragment()
+        val tr = supportFragmentManager.beginTransaction()
+        tr.add(R.id.fragment_container, movieDetails)
+        tr.commit()
     }
 }
---
+++ app/src/main/java/com/io/moviO/Movie.kt
@@ -0,0 +1,12 @@
+package com.io.moviO
+
+import java.sql.Array
+
+data class Movie(
+    val name: String,
+    val poster: Int,
+    val year: String,
+    val gerne: String,
+    val cast: String,
+    val overview: String
+)

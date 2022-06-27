fad0ce773b5df9ef3452646aaac0dea7b3a79864
A20Karlikov <al.karlikov@gmail.com>
2022-06-27 10:42:40 +0300

    Movie Details Screen commit after comments

--- app/src/main/java/com/io/moviO/MainActivity.kt
+++ app/src/main/java/com/io/moviO/MainActivity.kt
@@ -2,6 +2,7 @@ package com.io.moviO
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.io.moviO.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,11 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val view = binding.root
+        setContentView(view)
 
         val movieDetails = MovieDetailsFragment()
         val tr = supportFragmentManager.beginTransaction()
-        tr.add(R.id.fragment_container, movieDetails)
+        tr.replace(R.id.fragment_container, movieDetails)
         tr.commit()
     }
 }

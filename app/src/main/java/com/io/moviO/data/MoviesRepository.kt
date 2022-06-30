902b9c970644513cbe3dc29adedcc124d91baf05
A20Karlikov <al.karlikov@gmail.com>
2022-06-30 15:44:25 +0300

    refactor movie list in repository

--- app/src/main/java/com/io/moviO/data/MoviesRepository.kt
+++ app/src/main/java/com/io/moviO/data/MoviesRepository.kt
@@ -4,104 +4,101 @@ import com.io.moviO.R
 
 object MoviesRepository {
 
-    private var moviesData = mutableListOf<Movie>()
+    private val moviesData: List<Movie> = mutableListOf(
+        Movie(
+            "1",
+            "Movie 1",
+            R.drawable.ic_launcher_background,
+            "2000",
+            "Drama",
+            "Cast Crew",
+            "Overview"
+        ),
+        Movie(
+            "2",
+            "Movie 2",
+            R.drawable.ic_launcher_background,
+            "2001",
+            "Comedy",
+            "Cast Crew",
+            "Overview"
+        ),
+        Movie(
+            "3",
+            "Movie 3",
+            R.drawable.ic_launcher_background,
+            "2002",
+            "Drama",
+            "Cast Crew",
+            "Overview"
+        ),
+        Movie(
+            "4",
+            "Movie 4",
+            R.drawable.ic_launcher_background,
+            "2003",
+            "Comedy",
+            "Cast Crew",
+            "Overview"
+        ),
+        Movie(
+            "5",
+            "Movie 5",
+            R.drawable.ic_launcher_background,
+            "2004",
+            "Drama",
+            "Cast Crew",
+            "Overview"
+        ),
+        Movie(
+            "6",
+            "Movie 6",
+            R.drawable.ic_launcher_background,
+            "2005",
+            "Comedy",
+            "Cast Crew",
+            "Overview"
+        ),
+        Movie(
+            "7",
+            "Movie 7",
+            R.drawable.ic_launcher_background,
+            "2006",
+            "Drama",
+            "Cast Crew",
+            "Overview"
+        ),
+        Movie(
+            "8",
+            "Movie 8",
+            R.drawable.ic_launcher_background,
+            "2007",
+            "Comedy",
+            "Cast Crew",
+            "Overview"
+        ),
+        Movie(
+            "9",
+            "Movie 9",
+            R.drawable.ic_launcher_background,
+            "2008",
+            "Drama",
+            "Cast Crew",
+            "Overview"
+        ),
+        Movie(
+            "10",
+            "Movie 10",
+            R.drawable.ic_launcher_background,
+            "2009",
+            "Comedy",
+            "Cast Crew",
+            "Overview"
+        )
+    )
 
     fun getMovies(): List<Movie> {
         return moviesData
     }
 
-    init {
-        moviesData = mutableListOf(
-            Movie(
-                "1",
-                "Movie 1",
-                R.drawable.ic_launcher_background,
-                "2000",
-                "Drama",
-                "Cast Crew",
-                "Overview"
-            ),
-            Movie(
-                "2",
-                "Movie 2",
-                R.drawable.ic_launcher_background,
-                "2001",
-                "Comedy",
-                "Cast Crew",
-                "Overview"
-            ),
-            Movie(
-                "3",
-                "Movie 3",
-                R.drawable.ic_launcher_background,
-                "2002",
-                "Drama",
-                "Cast Crew",
-                "Overview"
-            ),
-            Movie(
-                "4",
-                "Movie 4",
-                R.drawable.ic_launcher_background,
-                "2003",
-                "Comedy",
-                "Cast Crew",
-                "Overview"
-            ),
-            Movie(
-                "5",
-                "Movie 5",
-                R.drawable.ic_launcher_background,
-                "2004",
-                "Drama",
-                "Cast Crew",
-                "Overview"
-            ),
-            Movie(
-                "6",
-                "Movie 6",
-                R.drawable.ic_launcher_background,
-                "2005",
-                "Comedy",
-                "Cast Crew",
-                "Overview"
-            ),
-            Movie(
-                "7",
-                "Movie 7",
-                R.drawable.ic_launcher_background,
-                "2006",
-                "Drama",
-                "Cast Crew",
-                "Overview"
-            ),
-            Movie(
-                "8",
-                "Movie 8",
-                R.drawable.ic_launcher_background,
-                "2007",
-                "Comedy",
-                "Cast Crew",
-                "Overview"
-            ),
-            Movie(
-                "9",
-                "Movie 9",
-                R.drawable.ic_launcher_background,
-                "2008",
-                "Drama",
-                "Cast Crew",
-                "Overview"
-            ),
-            Movie(
-                "10",
-                "Movie 10",
-                R.drawable.ic_launcher_background,
-                "2009",
-                "Comedy",
-                "Cast Crew",
-                "Overview"
-            )
-        )
-    }
 }

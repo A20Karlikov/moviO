f40a3b2ae5147122140c57f53b2b532fe19bf42a
A20Karlikov <al.karlikov@gmail.com>
2022-07-01 17:55:26 +0300

    refactor DataResult class

--- app/src/main/java/com/io/moviO/data/DataResult.kt
+++ app/src/main/java/com/io/moviO/data/DataResult.kt
@@ -5,13 +5,4 @@ sealed class DataResult<out R> {
     data class Success<out T>(val value: T) : DataResult<T>()
     data class Fail(val exception: Exception) : DataResult<Nothing>()
 
-    override fun toString(): String {
-        return when (this) {
-            is Success<*> -> "Success[value=$value]"
-            is Fail -> "Error[exception=$exception]"
-        }
-    }
-
-    val DataResult<*>.successed get() = this is Success<*> && value != null
-
 }
